b4fff0a527cfcdca3261e9424e685328b475a507
Kim-DaHam <times7749@naver.com>
2022-09-02 13:49:37 +0900

    project create and setting

---
+++ react-app/src/components/App.js
@@ -0,0 +1,12 @@
+import AppRouter from './Router'
+import { useEffect, useState } from "react"
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/routes/Home.js
@@ -0,0 +1,15 @@
+import Header from './components/header.js';
+import mainContent from './components/main-content.js';
+import Footer from './components/footer.js';
+
+function Home(){
+    return(
+        <>
+            <Header></Header>
+            <mainContent></mainContent>
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default Home;
