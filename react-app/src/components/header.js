55882e3cb13928951136164d02f1fdbbe2ac0972
Kim-DaHam <times7749@naver.com>
2022-09-02 14:29:23 +0900

    header

--- react-app/src/components/header.js
+++ react-app/src/components/header.js
@@ -1,8 +1,47 @@
+import "../stylesheets/style.css";
+
 function header(){
     return(
-        <>
-            <h1>헤더</h1>
-        </>
+        <div id="Header">
+            /* --miniBar-- */
+            <div id="miniBar">
+                <ul>
+                    <li><a href="#">로그인</a></li>
+                    <li><a href="#">회원가입</a></li>
+                    <li><a href="#">instagram</a></li>
+                    <li><a href="#">facebook</a></li>
+                </ul>
+            </div>
+            <br/>
+            /* --NavBar-- */
+            <div id="NavBar">
+                <nav id="navBody" class="navbar navbar-expand-sm bg-light navbar-light">
+                    <a id="Logo" href="#" class="navbar-brand"><img src="./images/Logo.JPG"></a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">BRAND</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">ESG</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">MENU</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">STORE</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">EVENT</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">FRANCHISE</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">HANSOT</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="collapse" id="collapse-menu">
+                <ul>
+                    <li><a class="dropdown-item" href="#">전체메뉴</a></li>
+                    <li><a class="dropdown-item" href="#">식재료 이야기</a></li>
+                    <li><a class="dropdown-item" href="#">페루찬차마요커피</a></li>
+                    <li><a class="dropdown-item" href="#">단체 주문</a></li>
+                </ul>
+                <ul>
+                    <li><a class="dropdown-item" href="#">한솥의 약속</a></li>
+                    <li><a class="dropdown-item" href="#">오시는 길</a></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
