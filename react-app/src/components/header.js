bced8bb875903f1ba350e9426022a674071f6f06
Kim-DaHam <times7749@naver.com>
2022-09-02 15:21:16 +0900

    header ui

--- react-app/src/components/header.js
+++ react-app/src/components/header.js
@@ -18,25 +18,25 @@ function header(){
                 <nav id="navBody" class="navbar navbar-expand-sm">
                     <a id="Logo" href="#" class="navbar-brand"><img src="images/Logo.jpg"/></a>
                     <ul class="navbar-nav">
-                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">BRAND</a></li>
-                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">ESG</a></li>
-                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">MENU</a></li>
-                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">STORE</a></li>
-                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">EVENT</a></li>
-                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">FRANCHISE</a></li>
-                        <li class="nav-item"><a href="#" data-toggle="collapse" data-target="#collapse-menu">HANSOT</a>
+                        <li class="nav-item"><a href="#" data-toggle="collapse">BRAND</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse">ESG</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse">MENU</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse">STORE</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse">EVENT</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse">FRANCHISE</a></li>
+                        <li class="nav-item"><a href="#" data-toggle="collapse">HANSOT</a>
                         </li>
                     </ul>
                 </nav>
             </div>
             <div class="collapse" id="collapse-menu">
-                <ul>
+                <ul id="menu-detail">
                     <li><a class="dropdown-item" href="#">전체메뉴</a></li>
                     <li><a class="dropdown-item" href="#">식재료 이야기</a></li>
                     <li><a class="dropdown-item" href="#">페루찬차마요커피</a></li>
                     <li><a class="dropdown-item" href="#">단체 주문</a></li>
                 </ul>
-                <ul>
+                <ul id="hansot-detail">
                     <li><a class="dropdown-item" href="#">한솥의 약속</a></li>
                     <li><a class="dropdown-item" href="#">오시는 길</a></li>
                 </ul>
