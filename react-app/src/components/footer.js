86b5d9544a6f711fad6fa2ceed3220604a9e4b6a
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2022-09-04 03:52:55 +0900

    header&content ui

--- react-app/src/components/footer.js
+++ react-app/src/components/footer.js
@@ -1,9 +1,29 @@
-function footer(){
+import "../stylesheets/footer.css";
+
+function Footer(){
     return(
         <>
-            <h1>Footer</h1>
+        <footer class="w3-container w3-center w3-margin-top">
+            <ul id="footer-menu">
+                <li>인재채용</li>
+                <li>협력업체등록</li>
+                <li>공지사항</li>
+                <li>고객 센터</li>
+                <li>개인정보처리방침</li>
+                <li>이용약관</li>
+            </ul>
+            <div>web award korea 2017 프랜차이즈분야 대상2017</div>
+            <hr/>
+            <ul>
+                <li><b>소속:</b> 금오공과대학교 컴퓨터공학과</li>
+                <li><b>학번:</b> 20190117</li>
+                <li><b>이름:</b> 김다함</li>
+            </ul>
+            <p>공통프로젝트 - 클론코딩 - 한솥 웹사이트 제작</p>
+            <p>2022-08-07</p>
+        </footer>
         </>
     )
 }
 
-export default footer;
+export default Footer;
--- react-app/src/components/mainContent.js
+++ react-app/src/components/mainContent.js
@@ -25,116 +25,126 @@ function MainContent(){
                 </Carousel.Next>
             </Carousel>
 
-
-            <div id="content01" class="w3-row-padding">
-                <div id="c01-01" name="box" class="w3-third w3-container w3-margin-bottom">
-                    <img src="images/image01.jpg" class="w3-hover-opacity"/>
-                    <div class="w3-container">
-                        <p><b>가격 이상의 가치가 담긴 메뉴를 확인해보세요</b></p>
+            <div id="content01" className="w3-row-padding">
+                <div id="c01-01" className="w3-third w3-container">
+                    <a href="#"><img src="images/image01.jpg"/></a>
+                    <div className="w3-container">
+                        <br/>
+                        <h3>가격 이상의 가치가 담긴<br/>메뉴를 확인해보세요</h3>
+                        <br/>
                         <button>한솥 메뉴</button>
                     </div>
+                    <br></br>
                     <div name="black-bar"></div>
                 </div>
-                <div id="c01-02" name="box" class="w3-third w3-container w3-margin-bottom">
-                    <img src="images/image02.jpg" class="w3-hover-opacity"/>
-                    <div class="w3-container">
-                        <p><b>갓 지은 한 끼가 기다리고 있는 가까운 한솥 매장을 찾아보세요</b></p>
+                <div id="c01-02" className="w3-third w3-container">
+                    <a><img href="#" src="images/image02.jpg"/></a>
+                    <div className="w3-container">
+                        <br/>
+                        <h3>갓 지은 한 끼가 기다리고 있는<br/>가까운 한솥 매장을 찾아보세요</h3>
+                        <br/>
                         <button>주변점포찾기</button>
                     </div>
+                    <br></br>
                     <div name="black-bar"></div>
                 </div>
             </div>
-{/*
-            <div id="content02" class="w3-row-padding">
-                <div id="c02-01" name="text-box" class="w3-third w3-container w3-margin-bottom" style="height: 402px; background-color: rgb(255, 209, 0);">
-                    <div class="w3-container">
+
+            <div id="content02" className="w3-row-padding">
+                <div id="c02-01" name="text-box" className="w3-third w3-container w3-margin-bottom">
+                    <div className="w3-container">
                         <h2><b>가맹점주에게도<br/>든든한 한솥입니다</b></h2>
+                        <br/>
                         <p>점주님들의 꿈을 실현시켜 드리기 위해 가맹점주와 성공까지 동행하는 한솥!
                         상담부터 개업까지 모든 희로애락을 함께합니다.</p>
                     </div>
                 </div>
-                <div id="c02-02" name="box" class="w3-third w3-container w3-margin-bottom">
-                    <img src="images/image03.jpg" alt="Norway" class="w3-hover-opacity"/>
-                    <div class="w3-container">
-                        <p>한솥은 언제나 고객과 점주님들의 이익을 먼저 생각합니다</p>
+                <div id="c02-02" className="w3-third w3-container w3-margin-bottom">
+                    <a><img src="images/image03.jpg"/></a>
+                    <div className="w3-container">
+                        <br/>
+                        <h4>한솥은 언제나 고객과 점주님들의<br/>이익을 먼저 생각합니다</h4>
+                        <br/>
                         <button>Why 한솥 가맹점</button>
                     </div>
+                    <br/>
                     <div name="black-bar"></div>
                 </div>
-                <div name="box" id="c02-03" class="w3-third w3-container w3-margin-bottom">
-                    <img src="images/image04.jpg" alt="Norway" class="w3-hover-opacity"/>
-                    <div class="w3-container">
-                        <p>한솥과 함께하는 가맹점주들의 성공 스토리입니다</p>
+                <div id="c02-03" className="w3-third w3-container w3-margin-bottom">
+                    <a><img src="images/image04.jpg"/></a>
+                    <div className="w3-container">
+                        <br/>
+                        <h4>한솥과 함께하는 가맹점주들의<br/>성공 스토리입니다</h4>
+                        <br/>
                         <button>성공수기</button>
                     </div>
+                    <br/>
                     <div name="black-bar"></div>
                 </div>
             </div>
 
-            <div id="content03" class="w3-rows">
-                <div id="c03-01" name="box" class="w3-third w3-margin-bottom">
+            <div id="content03" className="w3-rows">
+                <div id="c03-01" name="box" className="w3-third w3-margin-bottom">
                     <img src="images/image05.jpg"/>
-                    <div id="c03-02" name="box" class="w3-third w3-container w3-margin-bottom">
-                        <div class="w3-container">
-                            <h2><b>든든한 솥밥을 위해<br/>한솥이 지키고,<br/>키워가는 것들</b></h2>
+                    <div id="c03-02" name="box" className="w3-third w3-container w3-margin-bottom">
+                        <div className="w3-container">
+                            <h1><b>든든한 솥밥을 위해<br/>한솥이 지키고,<br/>키워가는 것들</b></h1>
                             &nbsp;
+                            <br/>
                             <p>
                                 한솥은 '따끈한 도시락으로 지역사회에 공헌한다' 라는<br/>기업이념 아래,
                                 고객 이익을 최우선으로 하며 엄선된<br/>좋은 식재료만 사용하는
                                 대한민국 외식종합기업 시장을<br/>리드하는 글로벌 종합외식기업 입니다.
                             </p>
+                            <br/>
                             <button>브랜드 철학</button>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div id="content04" class="w3-row-padding">
-                <div id="c04-01" name="box" class="w3-third w3-container w3-margin-bottom">
+            <div id="content04" className="w3-row-padding">
+                <div id="c04-01" name="box" className="w3-third w3-container w3-margin-bottom">
                     <img src="images/image06.jpg"/>
                 </div>
-                <div id="c04-02" name="box" class="w3-third w3-container w3-margin-bottom">
-                    <div class="w3-container">
+                <div id="c04-02" name="box" className="w3-third w3-container w3-margin-bottom">
+                    <div className="w3-container">
                         <h1><b>식재료 앞에서는<br/>한없이<br/>까탈스럽습니다</b></h1>
+                        <br/>
                         <p>
                             맑고 깨끗한 자연 환경속에서 재배된 식재료가 아니면
                             한솥이 될 수 없습니다. 모든 식재료는 고객분들이 안심하고
                             드실 수 있도록 엄격한 기준 아래 선별됩니다.
-                        </p>
+                        </p><br/>
                         <button>식재료이야기</button>
                     </div>
+                    <br/>
                     <div name="black-bar"></div>
                 </div>
             </div>
 
-            <div id="content05" class="w3-row-padding">
+            <div id="content05" className="w3-row-padding">
                 <h4><b>더 많은 이야기</b></h4>
-                <div class="w3-third w3-container w3-margin-bottom">
+                <br/>
+                <div className="w3-third w3-container w3-margin-bottom">
                     <img src="images/image07.jpg"/>
-                    <div class="w3-container w3-white">
-                        <p>
-                            한솥의 약속
-                        </p>
+                    <div className="w3-container w3-white">
+                        <p>한솥의 약속</p>
                     </div>
                 </div>
-                <div class="w3-third w3-container w3-margin-bottom">
+                <div className="w3-third w3-container w3-margin-bottom">
                     <img src="images/image08.jpg"/>
-                    <div class="w3-container w3-white">
-                        <p>
-                            온라인 이벤트
-                        </p>
+                    <div className="w3-container w3-white">
+                        <p>온라인 이벤트</p>
                     </div>
                 </div>
-                <div class="w3-third w3-container w3-margin-bottom">
+                <div className="w3-third w3-container w3-margin-bottom">
                     <img src="images/image09.jpg"/>
-                    <div class="w3-container w3-white">
-                        <p>
-                            한솥 NEWS
-                        </p>
+                    <div className="w3-container w3-white">
+                        <p>한솥 NEWS</p>
                     </div>
                 </div>
-        </div>
-    */}
+            </div>
         </div>
     )
 }
