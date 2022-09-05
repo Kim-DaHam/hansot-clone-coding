b7401f2be4a8043185b030bff82cd0636cf21e7b
Kim-DaHam <times7749@naver.com>
2022-09-05 11:24:15 +0900

    after cherrypick

--- react-app/src/components/LeftMenu.js
+++ react-app/src/components/LeftMenu.js
@@ -3,9 +3,6 @@ import "../stylesheets/left_menu.css";
 import { React } from 'bootstrap-4-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Accordion } from "react-bootstrap";
-import categoryData from '../data/category.js'
-import menuData from '../data/menu.js'
-import { useEffect, useState } from "react";
 
 function LeftMenu() {
 
@@ -18,16 +15,52 @@ function LeftMenu() {
         <Accordion className="sidenav" defaultActiveKey="0" flush>
           <ul id="side-menu">
             <Accordion.Item eventKey="0">
-              <Accordion.Header><b>전체메뉴</b></Accordion.Header>
+              <Accordion.Header>전체메뉴</Accordion.Header>
             </Accordion.Item>
-            {
-              console.log(category)
-            }
             <Accordion.Item eventKey="1">
-              <Accordion.Header><b>신메뉴/행사</b></Accordion.Header>
+              <Accordion.Header>신메뉴/행사</Accordion.Header>
               <Accordion.Body>
                 <ul>
-                  <li><a href="#">신메뉴</a></li>
+                  <li>신메뉴</li>
+                </ul>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>프리미엄·고메이</Accordion.Header>
+              <Accordion.Body>
+                <ul>
+                  <li>프리미엄</li>
+                </ul>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>사각도시락</Accordion.Header>
+              <Accordion.Body>
+                <ul>
+                  <li>고기고기시리즈</li>
+                  <li>모둠시리즈</li>
+                  <li>정식시리즈</li>
+                </ul>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+              <Accordion.Header>보울도시락</Accordion.Header>
+              <Accordion.Body>
+                <ul>
+                  <li>마요</li>
+                  <li>카레</li>
+                  <li>볶음밥</li>
+                  <li>프리미엄 찌개/탕</li>
+                </ul>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5">
+              <Accordion.Header>실속반찬/사이드</Accordion.Header>
+              <Accordion.Body>
+                <ul>
+                  <li>실속반찬</li>
+                  <li>스낵 시리즈</li>
+                  <li>미니반찬</li>
                 </ul>
               </Accordion.Body>
             </Accordion.Item>
