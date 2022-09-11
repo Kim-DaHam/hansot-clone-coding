a447fa03dd58fbbfa8abcdb07fd2af1a5c0b50a4
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2022-09-11 10:18:57 +0900

    rename footer header

--- react-app/src/components/index.js
+++ react-app/src/components/index.js
@@ -1,5 +1,5 @@
-export {default as Header } from '../components/header/Header.jsx';
-export {default as Footer } from '../components/footer/Footer.jsx';
+export {default as Header } from './header/Header.jsx';
+export {default as Footer } from './footer/Footer.jsx';
 export {default as MainContent } from '../components/mainContent/MainContent.jsx';
 export {default as LeftMenu } from '../components/leftMenu/LeftMenu.jsx';
 export {default as MenuList } from '../components/menuList/MenuList.jsx';
